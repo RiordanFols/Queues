b852c844b5fd6c8e68cb988845129fc6254ec026
Pavel Chernov <p.chernov@itigris.ru>
2023-04-04 13:06:49 +0300

    add map of topics

--- src/main/java/ru/chernov/queues/service/queue/QueueService.java
+++ src/main/java/ru/chernov/queues/service/queue/QueueService.java
@@ -4,31 +4,54 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.stereotype.Service;
+import ru.chernov.queues.config.properties.QueueProperties;
+import ru.chernov.queues.config.properties.QueueProperties.QueueConfig;
 import ru.chernov.queues.exception.QueueReadException;
+import ru.chernov.queues.exception.QueueTopicNotFoundException;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 import java.util.Queue;
 import java.util.concurrent.ArrayBlockingQueue;
 
 
+/**
+ * Для топиков лучше не создавать enum, чтобы можно было докидывать новые топики без изменения кода приложения
+ */
 @Service
-public class QueueService {
+public class QueueService implements InitializingBean {
     private static final Logger logger = LogManager.getLogger(QueueService.class);
-    private static final Integer QUEUE_SIZE = 1_000;
-    // TODO: map of queues
-    private static final Queue<Object> QUEUE = new ArrayBlockingQueue<>(QUEUE_SIZE);
+    private static final Map<String, Queue<Object>> QUEUES = new HashMap<>();
 
+    private final QueueProperties queueProperties;
     private final ObjectMapper objectMapper;
 
 
-    public QueueService(ObjectMapper objectMapper) {
+    public QueueService(QueueProperties queueProperties, ObjectMapper objectMapper) {
+        this.queueProperties = queueProperties;
         this.objectMapper = objectMapper;
     }
 
 
-    public <T> Optional<T> consume(Class<T> clazz) {
-        Object object = QUEUE.poll();
+    @Override
+    public void afterPropertiesSet() {
+        Map<String, QueueConfig> queueConfigs = queueProperties.getConfigs();
+        for (Map.Entry<String, QueueConfig> configEntry : queueConfigs.entrySet()) {
+            var topic = configEntry.getKey();
+            var config = configEntry.getValue();
+            var queue = new ArrayBlockingQueue<>(config.getSize());
+            QUEUES.put(topic, queue);
+        }
+    }
+
+
+    public <T> Optional<T> consume(String topic, Class<T> clazz) {
+        Queue<Object> queue = get(topic);
+        var object = queue.poll();
+
         if (object == null) {
             return Optional.empty();
         }
@@ -42,9 +65,17 @@ public class QueueService {
     }
 
 
-    public void produce(Object object) {
-        QUEUE.add(object);
+    public void produce(String topic, Object object) {
+        Queue<Object> queue = get(topic);
+        queue.add(object);
     }
 
+
+    private Queue<Object> get(String topic) {
+        return Optional.ofNullable(QUEUES.get(topic)).orElseThrow(() -> {
+            logger.error("Queue with topic [{}] not found", topic);
+            throw new QueueTopicNotFoundException();
+        });
+    }
 
 }
--- src/test/java/ru/chernov/queues/QueueTest.java
+++ src/test/java/ru/chernov/queues/QueueTest.java
@@ -12,34 +12,43 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 // TODO: more tests
 public class QueueTest extends AbstractTest {
+    private static final String RECEIPTS_TOPIC = "receipts";
+    private static final String NEWS_TOPIC = "news";
     private static final Random RANDOM = new Random();
     private static final Integer ITERATIONS = 15;
 
 
     @Test
     void simulation() throws InterruptedException {
-        ExecutorService executorService = Executors.newFixedThreadPool(2);
-        executorService.submit(produce());
-        executorService.submit(consume());
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        
+        executorService.submit(produce(RECEIPTS_TOPIC));
+        executorService.submit(consume(RECEIPTS_TOPIC));
+
+        executorService.submit(produce(NEWS_TOPIC));
+        executorService.submit(consume(NEWS_TOPIC));
+        executorService.submit(consume(NEWS_TOPIC));
+        executorService.submit(consume(NEWS_TOPIC));
+
         executorService.shutdown();
         boolean terminated = executorService.awaitTermination(1, TimeUnit.MINUTES);
         assertTrue(terminated);
     }
 
 
-    private Runnable produce() {
+    private Runnable produce(String topic) {
         return () -> {
-            for (int i = 0; i < ITERATIONS; i++) {
-                queueService.produce(RANDOM.nextInt(10));
+            for (var i = 0; i < ITERATIONS; i++) {
+                queueService.produce(topic, RANDOM.nextInt(10));
             }
         };
     }
 
 
-    private Runnable consume() {
+    private Runnable consume(String topic) {
         return () -> {
-            for (int i = 0; i < ITERATIONS; i++) {
-                queueService.consume(Integer.class).ifPresent(System.out::println);
+            for (var i = 0; i < ITERATIONS; i++) {
+                queueService.consume(topic, Integer.class).ifPresent(System.out::println);
             }
         };
     }
