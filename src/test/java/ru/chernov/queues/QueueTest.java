8d317687e356cd96902f928af92497370421330b
Pavel Chernov <p.chernov@itigris.ru>
2023-04-05 13:35:49 +0300

    add tests

--- src/test/java/ru/chernov/queues/QueueTest.java
+++ src/test/java/ru/chernov/queues/QueueTest.java
@@ -10,7 +10,7 @@ import java.util.concurrent.TimeUnit;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
-// TODO: more tests
+// TODO: отлавливать эксепшены в потоках
 public class QueueTest extends AbstractTest {
     private static final String RECEIPTS_TOPIC = "receipts";
     private static final String NEWS_TOPIC = "news";
@@ -19,16 +19,43 @@ public class QueueTest extends AbstractTest {
 
 
     @Test
-    void simulation() throws InterruptedException {
-        ExecutorService executorService = Executors.newFixedThreadPool(10);
-        
-        executorService.submit(produce(RECEIPTS_TOPIC));
-        executorService.submit(consume(RECEIPTS_TOPIC));
+    void produce() {
+        produce(NEWS_TOPIC).run();
+    }
 
+
+    @Test
+    void consumeEmpty() {
+        consume(NEWS_TOPIC).run();
+    }
+
+
+    @Test
+    void produceAndConsume() {
+        produce(NEWS_TOPIC).run();
+        consume(NEWS_TOPIC).run();
+    }
+
+
+    @Test
+    void produceAndConsumeOneTopicParallel() throws InterruptedException {
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
         executorService.submit(produce(NEWS_TOPIC));
         executorService.submit(consume(NEWS_TOPIC));
+
+        executorService.shutdown();
+        boolean terminated = executorService.awaitTermination(1, TimeUnit.MINUTES);
+        assertTrue(terminated);
+    }
+
+
+    @Test
+    void produceAndConsumeTwoTopicsParallel() throws InterruptedException {
+        ExecutorService executorService = Executors.newFixedThreadPool(4);
+        executorService.submit(produce(NEWS_TOPIC));
+        executorService.submit(produce(RECEIPTS_TOPIC));
         executorService.submit(consume(NEWS_TOPIC));
-        executorService.submit(consume(NEWS_TOPIC));
+        executorService.submit(consume(RECEIPTS_TOPIC));
 
         executorService.shutdown();
         boolean terminated = executorService.awaitTermination(1, TimeUnit.MINUTES);
